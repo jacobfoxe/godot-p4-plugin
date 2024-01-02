5bc4eec96771dcc80660f1f7c7ee645c8a1e0c36
Jacob Foxe <fokkushi@gmail.com>
2024-01-02 12:54:27 -0500

    Added gdlibrary for plugin init

---
+++ src/gdlibrary.cpp
@@ -0,0 +1,31 @@
+#include "perforce_plugin.h"
+
+#include "godot_cpp/core/class_db.hpp"
+#include "godot_cpp/godot.hpp"
+
+void initialize_perforce_plugin_module(godot::ModuleInitializationLevel p_level) {
+	if (p_level != godot::MODULE_INITIALIZATION_LEVEL_EDITOR) {
+		return;
+	}
+
+	godot::ClassDB::register_class<PerforcePlugin>();
+}
+
+void uninitialize_perforce_plugin_module(godot::ModuleInitializationLevel p_level) {
+	if (p_level != godot::MODULE_INITIALIZATION_LEVEL_EDITOR) {
+		return;
+	}
+}
+
+extern "C" {
+
+GDExtensionBool GDE_EXPORT git_plugin_init(const GDExtensionInterfaceGetProcAddress p_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_address, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_perforce_plugin_module);
+	init_obj.register_terminator(uninitialize_perforce_plugin_module);
+	init_obj.set_minimum_library_initialization_level(godot::MODULE_INITIALIZATION_LEVEL_EDITOR);
+
+	return init_obj.init();
+}
+}
